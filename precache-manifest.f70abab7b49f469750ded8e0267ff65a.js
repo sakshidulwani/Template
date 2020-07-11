23538a32ade707be4061e698a18048e11f5be384
sakshidulwani <dulwanisakshi@gmail.com>
2020-07-11 20:21:58 +0530

    Updates

---
+++ precache-manifest.f70abab7b49f469750ded8e0267ff65a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8be7513993345e01ed4c039fb505837",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "485814e5882efd5192b8",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b173423781e4afadb94c",
+    "url": "/portfolio/static/css/main.9c141181.chunk.css"
+  },
+  {
+    "revision": "485814e5882efd5192b8",
+    "url": "/portfolio/static/js/2.f6c0255b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.f6c0255b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b173423781e4afadb94c",
+    "url": "/portfolio/static/js/main.ff235e22.chunk.js"
+  },
+  {
+    "revision": "0bb3a1d664a8010b31ce",
+    "url": "/portfolio/static/js/runtime-main.754f3e05.js"
+  }
+]);
